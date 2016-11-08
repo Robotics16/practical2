6594d9066338964687c469a91fef04dbafeb4445
Balint Rikker <rikkerb@gmail.com>
2016-11-08 17:04:35 +0000

    code cleanup

--- motor_square.py
+++ motor_square.py
@@ -5,35 +5,19 @@ import sys
 import random
 import math
 
-interface=brickpi.Interface()
-#interface.startLogging('logfile.txt')
-interface.initialize()
-
-# Setup motors
-motors = [0,3]
-speed = 5
-interface.motorEnable(motors[0])
-interface.motorEnable(motors[1])
-motorParams = interface.MotorAngleControllerParameters()
-motor_util.set_pid(motorParams)
-interface.setMotorAngleControllerParameters(motors[0],motorParams)
-interface.setMotorAngleControllerParameters(motors[1],motorParams)
-
-# Setup initial state of particles
-numberOfParticles = 100
-startPos = 300
-particles = [((startPos, startPos, 0), 1 / float(numberOfParticles)) for i in range(numberOfParticles)]
+startPos = 0.0
 
 def drawParticles(particles):
-    print "drawParticles:" + str([p[0] for p in particles])
+    offset = 300   # Offset used to draw on a sensible location on the screen
+    print "drawParticles:" + str([(x + offset, y + offset, th) for ((x, y, th), w) in particles])
 
 def updateParticlesForward(particles, d):
-    """Update all particles with 10cm forward motion, 
+    """Update all particles with d cm forward motion, 
     based on current theta for each."""
-	
+    
     for i in range(0, len(particles)):
-	updatedParticle = updateOneParticleForward(particles[i], d)
-	particles[i] = updatedParticle
+        updatedParticle = updateOneParticleForward(particles[i], d)
+        particles[i] = updatedParticle
 
     return particles
 
@@ -41,31 +25,32 @@ def updateParticlesRotate(particles, angle):
     """Update all particles with a certain angle"""
 
     for i in range(0, len(particles)):
-    	updatedParticle = updateOneParticleRotate(particles[i], angle)
-    	particles[i] = updatedParticle
+        updatedParticle = updateOneParticleRotate(particles[i], angle)
+        particles[i] = updatedParticle
 
     return particles
 
 def updateOneParticleForward(particle, d):
-    """Update particle triple with 10cm forward motion,
-    use gaussian distribution with mu=0 and estimated sigma"""
+    """Update particle triple with d cm forward motion,
+    use gaussian distribution with mu = 0 and estimated sigma"""
     ((old_x, old_y, old_theta), w) = particle
 
-    sigma_offset = 1   # estimated sigma 2cm2 TODO
-    sigma_rotation = 0.2 # TODO
+    sigma_offset   = 0.2     # estimated sigma for 10 cms (scaled with variance for longer distances) TODO
+    sigma_rotation = 0.2     # TODO
     mu = 0
 
-    e = random.gauss(mu, sigma_offset) # error term for coordinate offset
+    e = random.gauss(mu, math.sqrt((sigma_offset ** 2) * d / 10)) # error term for coordinate offset
     f = random.gauss(mu, sigma_rotation) # error term for rotation
     
     particle = ((old_x + (d + e) * math.cos(old_theta),
-                old_y + (d + e) * math.sin(old_theta),
-                old_theta + f), w) # TODO keep angle between -pi and pi
+                 old_y + (d + e) * math.sin(old_theta),
+                 old_theta + f), 
+                w) # TODO keep angle between -pi and pi
     
     return particle
 
 def updateOneParticleRotate(particle, angle):
-    """Update particle triple with 10cm forward motion,
+    """Update particle triple with angle radians rotation,
     use gaussian distribution with mu=0 and estimated sigma"""
     ((old_x, old_y, old_theta), w) = particle
                 
@@ -99,11 +84,11 @@ def navigateToWaypoint(w_x, w_y, particles):
     alpha = math.atan2(d_y, d_x) # absolute orientation needed (using atan2 so that the result is between -pi and pi)
     beta  = (alpha - theta) # angle to turn
     if (abs(beta) > math.pi) :
-    	if (beta > 0) :
+        if (beta > 0) :
             beta = beta - 2 * math.pi
         else :
             beta = beta + 2 * math.pi
-       
+
     motor_util.rotate(-beta, interface, motors)
     particles = updateParticlesRotate(particles, beta)
     print "Waypoint navigate: Rotate: " + str(beta)
@@ -115,29 +100,56 @@ def navigateToWaypoint(w_x, w_y, particles):
     particles = updateParticlesForward(particles, distance_to_move)
 
     (x, y, theta) = getCurrentLocation(particles)
-    print "Current loc: " + str((x-startPos, y-startPos, theta))
+    print "Current loc: " + str((x - startPos, y - startPos, theta))
 
+    
+def main():  
+    interface = brickpi.Interface()
+    #interface.startLogging('logfile.txt')
+    interface.initialize()
 
-"""# Go in squares
-for i in range(4):
-    for j in range(4):
-        motor_util.forward(10, interface, motors)
-        particles = updateParticlesForward(particles)
-        getCurrentLocation(particles)
+    # Setup motors
+    motors = [0,3]
+    speed = 5
+    interface.motorEnable(motors[0])
+    interface.motorEnable(motors[1])
+    motorParams = interface.MotorAngleControllerParameters()
+    motor_util.set_pid(motorParams)
+    interface.setMotorAngleControllerParameters(motors[0], motorParams)
+    interface.setMotorAngleControllerParameters(motors[1], motorParams)
+
+    # Setup initial state of particles
+    numberOfParticles = 100
+    particles = [((startPos, startPos, 0), 1 / float(numberOfParticles)) for i in range(numberOfParticles)]
+    
+    #Go in squares
+
+    for i in range(4):
+        for j in range(4):
+            #motor_util.forward(10, interface, motors)
+            drawParticles(particles)
+            time.sleep(0.25)
+            particles = updateParticlesForward(particles, 20)
+            drawParticles(particles)
+            time.sleep(0.25)
+        #motor_util.rotateRight90deg(interface, motors)
+        particles = updateParticlesRotate(particles, -math.pi/2)
         drawParticles(particles)
         time.sleep(0.25)
-    motor_util.rotateRight90deg(interface, motors)
-    particles = updateParticlesRotateLeft(particles)
-    getCurrentLocation(particles)
-    drawParticles(particles)
-    time.sleep(0.25)"""
 
-while(True):
-	w_x = float(input("Enter your desired Wx position: "))
-	w_y = float(input("Enter your desired Wy position: "))
-	navigateToWaypoint(w_x+startPos, w_y+startPos, particles) 
+    # Waypoint navigation:
+    #while(True):
+    #drawParticles(particles)
+    #w_x = float(input("Enter your desired Wx position: "))
+    #w_y = float(input("Enter your desired Wy position: "))
+    #navigateToWaypoint(w_x + startPos, w_y + startPos, particles)
+    #drawParticles(particles)
 
-print "Destination reached!"
+    print "Destination reached!"
 
-#interface.stopLogging('logfile.txt')
-interface.terminate()
+    #interface.stopLogging('logfile.txt')
+    interface.terminate()
+
+
+if __name__ == "__main__":
+    main()
